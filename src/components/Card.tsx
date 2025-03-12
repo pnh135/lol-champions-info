8349f09d717a67a58aebc7a67cfd5598f7d733f4
nahyun park <pnh13584@gmail.com>
2025-03-12 18:02:21 +0900

    Feat: Card 렌더링 로직 생성

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,16 @@
-const Card = () => {
-  return <></>;
+import Image from "next/image";
+
+const Card = ({ champions }) => {
+  return (
+    <div>
+      {champions.map((champ) => {
+        <div key={champ.name}>
+          <Image src={champ.image} alt="..." />
+          <div>{champ.plaintext}</div>
+        </div>;
+      })}
+    </div>
+  );
 };
 
 export default Card;

92347e889b6a7eb049efc4eda285b8b02461213c
nahyun park <pnh13584@gmail.com>
2025-03-13 12:26:28 +0900

    Feat: DetailPage로 연결된 Link 태그 생성

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,21 @@
 import { CHAMP_URL } from "@/constants/api";
 import { Champions } from "@/types/Champions";
 import Image from "next/image";
+import Link from "next/link";
 
 const Card = ({ champion }: { champion: Champions }) => {
   return (
-    <div className="bg-slate-200 border border-solid border-red-200 rounded-lg">
-      <Image
-        src={`${CHAMP_URL}${champion.image.full}`}
-        alt={champion.name}
-        width={100}
-        height={100}
-      />
-      <div>{champion.name}</div>
-      <div>{champion.title}</div>
+    <div className="bg-slate-200 border border-solid border-red-200 rounded-lg cursor-pointer">
+      <Link href={`/champions/${champion.id}`}>
+        <Image
+          src={`${CHAMP_URL}${champion.image.full}`}
+          alt={champion.name}
+          width={100}
+          height={100}
+        />
+        <div>{champion.name}</div>
+        <div>{champion.title}</div>
+      </Link>
     </div>
   );
 };

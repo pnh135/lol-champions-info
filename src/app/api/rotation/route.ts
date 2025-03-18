3969ed94e24cb8cb7c5d63d2c68403ae70bd8fb7
nahyun park <pnh13584@gmail.com>
2025-03-18 20:55:02 +0900

    Fix: api 오류 수정

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -4,11 +4,13 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-static";
 
+const API_KEY = process.env.RIOT_API_KEY as string;
+
 export async function GET() {
   const res = await fetch(`${ROTATION_URL}`, {
     method: "GET",
     headers: {
-      "X-Riot-Token": process.env.RIOT_API_KEY,
+      "X-Riot-Token": API_KEY,
     },
     next: {
       revalidate: 86400,

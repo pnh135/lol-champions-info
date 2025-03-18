2bb2436376577c465a2051207be5b037752dbf3f
nahyun park <pnh13584@gmail.com>
2025-03-18 20:06:49 +0900

    Style: homepage css 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="w-full text-center">
-      <div className="text-red-300">메인 페이지</div>
+      <div className="mb-4 text-white text-2xl mt-3">메인 페이지</div>
+      <div className="grid grid-cols-3 text-center items-center justify-center p-3">
+        <div className="bg-slate-200">
+          <p className="text-lg">가용 챔피언 목록</p>
+        </div>
+        <div className="bg-slate-200">
+          <p className="text-lg">가용 아이템 목록</p>
+        </div>
+        <div className="bg-slate-200">
+          <p className="text-lg">금주의 무료 챔피언</p>
+        </div>
+      </div>
     </div>
   );
 }

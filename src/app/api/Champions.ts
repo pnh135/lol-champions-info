06dac0f24b12d82004228111a13f68ff49b16727
nahyun park <pnh13584@gmail.com>
2025-03-18 14:31:23 +0900

    Fix: request 수정

--- src/app/api/Champions.ts
+++
@@ -1,8 +0,0 @@
-// champion 정보 json
-"https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json";
-
-// item 정보 json
-"https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/item.json";
-
-// champion 개별 정보 json
-"https://ddragon.leagueoflegends.com/cdn/15.5.1/data/en_US/champion/Aatrox.json";
--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -1,17 +1,20 @@
 import { ROTATION_URL } from "@/constants/api";
+import { NextResponse } from "next/server";
 
 export const dynamic = "force-static";
 
-export async function GET() {
+export async function GET(request: Request) {
   const res = await fetch(`${ROTATION_URL}`, {
-    "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Whale/4.30.291.11 Safari/537.36",
-    "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6",
-    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-    Origin: "https://developer.riotgames.com",
-    "X-Riot-Token": process.env.RIOT_API_KEY,
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Whale/4.30.291.11 Safari/537.36",
+      "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6",
+      "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+      Origin: "https://developer.riotgames.com",
+      "X-Riot-Token": process.env.RIOT_API_KEY,
+    },
   });
   const data = await res.json();
 
-  return Response.json({ data });
+  return NextResponse.json({ data });
 }

47f7c5dbe8de097f819cc979419213048a31eac3
nahyun park <pnh13584@gmail.com>
2025-03-19 09:09:32 +0900

    Feat: "rotation" usequery 저장

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,7 +1,51 @@
+"use client";
+
+import Card from "@/components/Card";
+import { CHAMPIONS_URL } from "@/constants/api";
+import { Champions } from "@/types/Champions";
+import { getChampionRotation } from "@/utils/riotApi";
+import { useQuery } from "@tanstack/react-query";
+
 const RotationPage = () => {
+  const {
+    data: rotationData,
+    isPending,
+    error,
+  } = useQuery({
+    queryKey: ["rotations"],
+    queryFn: async () => {
+      const { Rotationdata } = await getChampionRotation();
+      return Rotationdata;
+    },
+    staleTime: 60 * 60 * 24,
+  });
+
+  if (isPending) {
+    return <div>Loading ... </div>;
+  }
+
+  if (error) {
+    return <div>데이터를 가져오는 도중 오류가 발생하였습니다.</div>;
+  }
+
+  const Championdata = async () => {
+    const res = await fetch(`${CHAMPIONS_URL}`);
+    const fetchData = await res.json();
+    const champions: Champions = Object.values(fetchData.data);
+    return champions;
+  };
+  const filterChampion = Championdata.filter((champion) =>
+    rotationData.freeChampionIds.includes(parseInt(champion.key))
+  );
+
   return (
     <>
-      <div>금주의 챔피언 페이지</div>
+      <div className="mb-4 text-white text-2xl mt-3">금주의 챔피언 페이지</div>
+      <div className="grid grid-cols-6 gap-4 justify-center text-center">
+        {filterChampion.map((champion) => (
+          <Card champion={champion} key={champion.id} />
+        ))}
+      </div>
     </>
   );
 };

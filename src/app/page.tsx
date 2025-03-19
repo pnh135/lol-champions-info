2406e0d21cb5ec482fd584e99f7ba80b23bbfe92
nahyun park <pnh13584@gmail.com>
2025-03-19 10:33:40 +0900

    Feat: homepage 영상 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,16 @@ export default function Home() {
   return (
     <div className="w-full text-center">
       <div className="mb-4 text-white text-2xl mt-3">메인 페이지</div>
-      <div className="grid grid-cols-3 text-center items-center justify-center p-3">
-        <div className="bg-slate-200">
-          <p className="text-lg">가용 챔피언 목록</p>
-        </div>
-        <div className="bg-slate-200">
-          <p className="text-lg">가용 아이템 목록</p>
-        </div>
-        <div className="bg-slate-200">
-          <p className="text-lg">금주의 무료 챔피언</p>
-        </div>
-      </div>
+      <iframe
+        className="w-full mt-10"
+        width={840}
+        height={473}
+        src="https://www.youtube.com/embed/r6zIGXun57U?si=0P1NVOURWNgkCCJ3?autoplay=1"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }

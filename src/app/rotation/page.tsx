3ada1673831c3390af7ecb5e3c0600d3e57b16de
nahyun park <pnh13584@gmail.com>
2025-03-19 10:51:35 +0900

    Add: rotationPage 주석 추가

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -7,6 +7,7 @@
 // import { useQuery } from "@tanstack/react-query";
 
 const RotationPage = () => {
+  // rotations 정보를 tanstack으로 캐싱하기
   // const {
   //   data: rotationData,
   //   isPending,
@@ -20,19 +21,23 @@ const RotationPage = () => {
   //   staleTime: 60 * 60 * 24,
   // });
 
+  // 불러오는 중일 때 로딩 중 표시
   // if (isPending) {
   //   return <div>Loading ... </div>;
   // }
 
+  // 불러오는 걸 실패할 시 실패 오류 출력
   // if (error) {
   //   return <div>데이터를 가져오는 도중 오류가 발생하였습니다.</div>;
   // }
 
+  // 기존 챔피언 데이터 fetch 하기
   // const Championdata = async () => {
   //   const res = await fetch(`${CHAMPIONS_URL}`);
   //   const fetchData = await res.json();
   //   const champions: Champions[] = Object.values(fetchData.data);
 
+  // "rotations" 정보와 비교하여 로테이션 챔피언 뽑기
   //   const filterChampion = champions.filter((champion) =>
   //     rotationData.freeChampionIds.includes(champion.key)
   //   );
@@ -43,6 +48,7 @@ const RotationPage = () => {
     <>
       <div className="mb-4 text-white text-2xl mt-3">금주의 챔피언 페이지</div>
       <div className="grid grid-cols-6 gap-4 justify-center text-center">
+        {/* 챔피언 렌더링 */}
         {/* {filterChampion.map((champion) => (
           <Card champion={champion} key={champion.id} />
         ))} */}
